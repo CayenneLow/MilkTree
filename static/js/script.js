27278fac5903a627c8f4eaeb92a9d79534e6a9f7
Khye Ean Low <lowkhyeean@gmail.com>
2019-11-23 02:27:34 +1100

    styling to skill result

--- static/js/script.js
+++ static/js/script.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             return;
         }
         var alreadyThere = 0;
-        $('#skill-results').children('p').each(function () {
+        $('#skill-results').children('li').each(function () {
             if (skill == this.innerText) {
                 alreadyThere = 1;
             }
@@ -41,7 +41,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
             return;
         }
 
-        skillElement = document.createElement("p")
+        skillElement = document.createElement("li")
+        skillElement.className = "badge badge-warning list-inline-item"
         skillElement.innerText = skill;
         $("#skill-results").append(skillElement);
     }
