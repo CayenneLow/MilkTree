27a9ff7472aebfc9a135c60e8141abe89cf1ec3a
Selina Chua <selina.a.chua@gmail.com>
2019-11-22 14:49:56 +1100

    Added project??

--- models/project.py
+++ models/project.py
@@ -1,8 +1,8 @@
-class project:
+class Project:
     def __init__(self, title, desc):
         self.title = title
         self.desc = desc
-        self.location = ""
+        self.location = None
         self.jobs = []
 
     def get_title():
@@ -11,4 +11,24 @@ class project:
     def set_title(title):
         self.title = title
     
-    def 
+    def get_desc():
+        return self.desc
+    
+    def set_desc(desc):
+        self.desc = desc 
+    
+    def get_location():
+        return self.location
+
+    def set_location(location):
+        self.location = location 
+    
+    def get_jobs():
+        return self.jobs 
+    
+    def add_job(Job job):
+        self.jobs.append(job)
+    
+    def delete_job(Job job):
+        self.jobs.remove(job)
+    
