81c7f1a9bfe2e79d6f551fc1759947827f2b79d7
Nicholas Duller <nickyduller@gmail.com>
2019-11-22 16:44:03 +1100

    before pull

--- models/project.py
+++ models/project.py
@@ -9,35 +9,34 @@ class Project:
         self.jobs = [None]
 
     def get_proj_id(self):
-        return self.id 
-    
+        return self.id
+
     def set_proj_id(self, id):
         self.id = id
 
     def get_title(self):
         return self.title
-    
+
     def set_title(self, title):
         self.title = title
-    
+
     def get_desc(self):
         return self.desc
-    
+
     def set_desc(self, desc):
-        self.desc = desc 
-    
+        self.desc = desc
+
     def get_location(self):
         return self.location
 
     def set_location(self, location):
-        self.location = location 
-    
+        self.location = location
+
     def get_jobs(self):
-        return self.jobs 
-    
+        return self.jobs
+
     def add_job(self, job):
         self.jobs.append(job)
-    
+
     def delete_job(self, job):
         self.jobs.remove(job)
-    
