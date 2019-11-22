cfaf67a28bddf5ad4e926a35a15ef66ad92e2dbc
Selina Chua <selina.a.chua@gmail.com>
2019-11-22 14:30:54 +1100

    Added project class.

---
+++ models/project.py
@@ -0,0 +1,14 @@
+class project:
+    def __init__(self, title, desc):
+        self.title = title
+        self.desc = desc
+        self.location = ""
+        self.jobs = []
+
+    def get_title():
+        return self.title
+    
+    def set_title(title):
+        self.title = title
+    
+    def 
