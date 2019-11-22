3907440bda779c3726240e61ab57b6c704883303
Nicholas Duller <nickyduller@gmail.com>
2019-11-22 15:39:23 +1100

    created new project tempalte

--- routes.py
+++ routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, request, redirect, url_for, abort
 from server import app
+#from models import *
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/create-new-project', method="POST")
-def create_new_project():
-    
+@app.route('/create-project', methods=["GET", "POST"])
+def create_project():
+    return render_template('create_project.html')
